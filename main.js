5b5713253adab7116f6ae58f4d5baf102a57daeb
Jennifer <jennifereneh05@gmail.com>
2024-05-12 03:49:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var meun = document.getElementById("menu");
+meun.style.display = "none";
+
+function togglemenu() {
+  if (meun.style.display == "none") {
+    meun.style.display = "block";
+  } else {
+    meun.style.display = "none";
+  }
+}
